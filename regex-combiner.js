8a19de54f5d8a0770a04ee61684adba9eca53d89
Nick Fisher <spadgos@gmail.com>
2015-01-13 01:37:07 +0100

    Stupid amount of changes:
    - interface changed to provide extra methods
    - linting added
    - modularised each step of the process
    - combined regex is optimized (slightly)

--- regex-combiner.js
+++ regex-combiner.js
@@ -1,97 +1,51 @@
-var ender = '***',
-    groupers = {
-      '{': '}',
-      '[': ']',
-      '(': ')'
-    },
-    modifiers = '*?+';
+var combineRegexes = require('./lib/combine-regexes');
 
-module.exports = combineRegexes;
+/**
+ * Regex Combiner
+ *
+ * Accepts an array of RegExps (or Strings which will be converted to RegExp), and returns an object which can act upon
+ * the regexes as a group.
+ *
+ * @param  {Array.<RegExp|String>} arr
+ * @return {Object}
+ */
+module.exports = function (arr) {
+  var regexes      = arr.map(toRegex),
+      combined     = combineRegexes(regexes),
+      indexOfBound = indexOf.bind(null, combined, regexes);
+  return {
+    /**
+     * Test a string to see if any of the source regexes match the given string.
+     * @param  {String}
+     * @return {Boolean}
+     */
+    test: combined.test.bind(combined),
 
-function combineRegexes(arr) {
-  var trie = {};
-  arr.map(reToString).forEach(setDeep, trie);
-  compress(trie);
-  return trieToRegex(trie);
-}
+    /**
+     * Get the index of the first source regex which matched.
+     * @param  {String}
+     * @return {Number} The index, or -1 if none found.
+     */
+    indexOf: indexOfBound,
 
-function reToString(regex) {
-  if (typeof regex === 'string') return regex;
-  var str = regex.toString();
-  return str.substring(1, str.length - 1).replace(/\\\//g, '/');
-}
-
-function tokenize(str) {
-  var i, l, char, out = [], currToken = '', closer;
-
-  for (i = 0, l = str.length; i < l; ++i) {
-    char = str.charAt(i);
-
-    if (char === '\\') {
-      currToken += char + str.charAt(++i);
-    } else if (!closer && (closer = groupers[char])) {
-      currToken = char;
-    } else if (char === closer) {
-      currToken += char;
-      closer = null;
-      if (char === '}') { // this group is a modifier
-        out[out.length - 1] += currToken;
-        currToken = '';
-        continue;
-      }
-    } else if (modifiers.indexOf(char) > -1) {
-      if (closer) {
-        currToken += char;
-      } else {
-        out[out.length - 1] += char;
-      }
-      continue;
-    } else {
-      currToken += char;
+    exec: function (str) {
+      var index = indexOfBound(str);
+      return index > -1 ? regexes[index].exec(str) : null;
     }
-    if (!closer) {
-      out.push(currToken);
-      currToken = '';
+  };
+};
+
+function indexOf(combined, regexes, str) {
+  if (combined.test(str)) {
+    for (var i = 0; i < regexes.length; ++i) {
+      if (regexes[i].test(str)) {
+        return i;
+      }
     }
   }
-  return out;
+  return -1;
 }
 
-function setDeep(str) {
-  tokenize(str).reduce(function (memo, char, ind, arr) {
-    if (!memo[char]) {
-      memo[char] = {};
-    }
-    if (ind === arr.length - 1) {
-      memo[char][ender] = {};
-    }
-    return memo[char];
-  }, this);
-}
-
-function compress(trie) {
-  Object.keys(trie).forEach(function (key) {
-    var subtrie = trie[key],
-        subkeys;
-    compress(subtrie);
-    subkeys = Object.keys(subtrie);
-    if (subkeys.length === 1) {
-      trie[key + subkeys[0]] = subtrie[subkeys[0]];
-      delete trie[key];
-    }
-  });
-}
-
-function trieToRegex(trie) {
-  return new RegExp(trieToRegexStr(trie));
-}
-
-function trieToRegexStr(trie) {
-  var keys = Object.keys(trie);
-  if (keys.length === 0) {
-    return '';
-  }
-  return '(?:' + keys.map(function (key) {
-    return key.replace(ender, '') + trieToRegexStr(trie[key]);
-  }).join('|') + ')';
+function toRegex(input) {
+  return RegExp(input);
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*globals it, describe, before */
+/*globals it, describe */
 var expect = require('expect.js'),
     regexCombiner = require('./regex-combiner');
 
@@ -18,45 +18,51 @@ describe('Regex Combiner', function () {
     /escaped ([\)\]]) chars/,
     /escaped ([\)\]]){3} chars/
   ]);
-
-  it('matches combined regexes', function () {
-    [
-      'plain text',
-      'withhh modifier',
-      'character classes',
-      'with groups',
-      'with more groups',
-      'with modddifiers in groupss modifiers in groupsss',
-      'sub',
-      'substrings',
-      'char counttts',
-      'nested exprassions',
-      'nested express',
-      'escaped ) chars',
-      'escaped ] chars',
-      'escaped ]]] chars'
-    ].forEach(function (testStr) {
-      expect(testStr).to.match(combination);
+  describe('API', function () {
+    ['test', 'indexOf', 'exec'].forEach(function (fnName) {
+      it('provides `' + fnName + '()`', function () {
+        expect(combination[fnName]).to.be.a('function');
+      });
     });
   });
 
-  it('doesn\'t match other strings', function () {
-    [
-      'plain',
-      'text',
-      'something with modifiers',
-      'su',
-      'char counttttts',
-      'escaped ]] chars'
-    ].forEach(function (testStr) {
-      expect(testStr).not.to.match(combination);
+  describe('.test()', function () {
+    it('matches combined regexes', function () {
+      [
+        'plain text',
+        'withhh modifier',
+        'character classes',
+        'with groups',
+        'with more groups',
+        'with modddifiers in groupss modifiers in groupsss',
+        'sub',
+        'substrings',
+        'char counttts',
+        'nested exprassions',
+        'nested express',
+        'escaped ) chars',
+        'escaped ] chars',
+        'escaped ]]] chars'
+      ].forEach(function (testStr) {
+        expect(combination.test(testStr)).to.be(true);
+      });
     });
-  });
 
-  it('compiles modifiers correctly', function () {
-    var combined;
-    expect(function () {
-      combined = regexCombiner([
+    it('doesn\'t match other strings', function () {
+      [
+        'plain',
+        'text',
+        'something with modifiers',
+        'su',
+        'char counttttts',
+        'escaped ]] chars'
+      ].forEach(function (testStr) {
+        expect(combination.test(testStr)).to.be(false);
+      });
+    });
+
+    it('compiles modifiers correctly', function () {
+      var combined = regexCombiner([
         /a/,
         /aa/,
         /a+/,
@@ -69,30 +75,82 @@ describe('Regex Combiner', function () {
         /a{,5}/,
         /a{3,}/
       ]);
-    }).to.not.throwException();
-    expect('aa').to.match(combined);
+      expect(combined.test('aaa')).to.be(true);
+    });
+
+    it('works with strings too', function () {
+      var combined = regexCombiner([
+        'foo+',
+        'food',
+        'foos',
+        'football',
+        /ba[rh]/,
+        /bard/
+      ]);
+      [
+        'foo',
+        'fooooo',
+        'football',
+        'food',
+        'foos',
+        'bar',
+        'bah',
+        'bard'
+      ].forEach(function (testStr) {
+        expect(combined.test(testStr)).to.be(true);
+        expect(combined.indexOf(testStr)).to.be.greaterThan(-1);
+        expect(combined.exec(testStr)).to.be.an('array');
+      });
+      expect(combined.test('fo')).to.be(false);
+      expect(combined.test('bat')).to.be(false);
+    });
+
+    it('creates character classes correctly', function () {
+      var combined = regexCombiner([
+        /aa/,
+        /a-/,
+        /ac/
+      ]);
+      expect(combined.test('aa')).to.be(true);
+      expect(combined.test('ac')).to.be(true);
+      expect(combined.test('a-')).to.be(true);
+      expect(combined.test('ab')).to.be(false);
+    });
+
+    it('combines character classes correctly', function () {
+      var combined = regexCombiner([
+        /a[a-c]/,
+        /a[-_]/,
+        /a[efghi]/
+      ]);
+      expect(combined.test('aa')).to.be(true);
+      expect(combined.test('ac')).to.be(true);
+      expect(combined.test('a-')).to.be(true);
+      expect(combined.test('a_')).to.be(true);
+      expect(combined.test('af')).to.be(true);
+      expect(combined.test('ad')).to.be(false);
+    });
   });
 
-  it('works with strings too', function () {
-    var combined = regexCombiner([
-      'foo+',
-      'food',
-      'football',
-      /ba[rh]/,
-      /bard/
-    ]);
-    [
-      'foo',
-      'fooooo',
-      'football',
-      'food',
-      'bar',
-      'bah',
-      'bard'
-    ].forEach(function (testStr) {
-      expect(testStr).to.match(combined);
+  describe('.indexOf()', function () {
+    it('returns the index of the first matching regex', function () {
+      expect(combination.indexOf('with groups')).to.be(3);
     });
-    expect('fo').not.to.match(combined);
-    expect('bat').not.to.match(combined);
+
+    it('returns -1 when no match is found', function () {
+      expect(combination.indexOf('zxcvbnm')).to.be(-1);
+    });
+  });
+
+  describe('.exec()', function () {
+    it('returns the matching groups of the first matched regex', function () {
+      var match = combination.exec('with groups yeah');
+      expect(match[0]).to.be('with groups');
+      expect(match[1]).to.be('groups');
+    });
+
+    it('returns null when no match is found', function () {
+      expect(combination.exec('zxcvbnm')).to.be(null);
+    });
   });
 });
